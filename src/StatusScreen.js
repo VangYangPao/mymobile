74defe32572459582e0e6baff68c10e4ee5e1308
mavenave <guanhao3797@gmail.com>
2017-06-27 05:32:56 +0800

    updated StatusScreen

--- src/StatusScreen.js
+++ src/StatusScreen.js
@@ -16,89 +16,97 @@ export default class StatusScreen extends Component {
     title: "My Policies"
   };
 
-  renderItem({ item, index }) {
-    const verbMap = {
-      active: "Purchased",
-      pending: "Claimed",
-      rejected: "Rejected"
+  renderItem(section, { item, index }) {
+    function getDateStr(datetime) {
+      const day = datetime.getDate();
+      const month = datetime.getMonth();
+      const year = datetime.getFullYear();
+      let hour = datetime.getHours();
+      const minute = datetime.getMinutes();
+      if (hour > 12) {
+        meridien = "PM";
+        hour -= 12;
+      } else {
+        meridien = "AM";
+      }
+      const dateStr = `${hour}:${minute}${meridien} ${day}-${month}-${year}`;
+      return dateStr;
+    }
+    const dateStr = getDateStr(item.purchaseDate);
+
+    const styleMap = {
+      active: styles.policyStatusTextActive,
+      expiring: styles.policyStatusTextExpiring,
+      expired: styles.policyStatusTextRejected,
+      pending: styles.policyStatusTextExpiring,
+      rejected: styles.policyStatusTextRejected
     };
-    const day = item.date.getDate();
-    const month = item.date.getMonth();
-    const year = item.date.getFullYear();
-    const dateStr = `${day}-${month}-${year}`;
+
     return (
       <View style={styles.policy}>
-        <Text style={styles.policyName}>{item.name}</Text>
-        <Text style={styles.date}>
-          {verbMap[item.status]} on: {dateStr}
-        </Text>
-        {item.status !== "active"
-          ? <Text style={styles.date}>
-              Amount: ${item.claimAmount + ""}
-            </Text>
-          : null}
+        <View style={styles.policyContent}>
+          <Text style={styles.policyName}>{item.name}</Text>
+          <Text style={styles.date}>Policy No: {item.key}</Text>
+          <Text style={styles.date}>
+            Purchased on: {dateStr}
+          </Text>
+          <Text style={styles.date}>
+            Amount: ${(section === "policies" ? item.paid : item.claimAmount) +
+              ""}
+          </Text>
+        </View>
+        <View style={styles.policyStatus}>
+          <Text style={[styles.policyStatusText, styleMap[item.status]]}>
+            {item.status.toUpperCase()}
+          </Text>
+        </View>
       </View>
     );
   }
 
   renderSectionHeader({ section }) {
-    const headerMap = {
-      active: "ACTIVE POLICIES",
-      pending: "PENDING CLAIMS",
-      rejected: "REJECTED/EXPIRED POLICIES"
-    };
-    const header = headerMap[section.key];
-    const rejectedStyle = section.key === "rejected"
-      ? styles.rejectedSectionHeader
-      : null;
     return (
-      <View style={[styles.sectionHeader, rejectedStyle]}>
-        <Text style={styles.sectionHeaderText}>{header}</Text>
+      <View style={styles.sectionHeader}>
+        <Text style={styles.sectionHeaderText}>{section.title}</Text>
       </View>
     );
   }
 
   render() {
-    const activePolicies = [
+    const policies = [
       {
-        key: 1,
+        key: "PL12",
         status: "active",
         name: "Accidental Death / Permanent Disability",
-        date: new Date(2017, 5, 26, 16, 43)
+        purchaseDate: new Date(2017, 5, 26, 16, 43),
+        paid: 5.66
       },
       {
-        key: 2,
+        key: "PL13",
         status: "active",
         name: "Accidental Death / Permanent Disability",
-        date: new Date(2017, 5, 26, 16, 43)
+        purchaseDate: new Date(2017, 5, 26, 16, 43),
+        paid: 2.68
       },
       {
-        key: 3,
-        status: "active",
+        key: "PL14",
+        status: "expired",
+        name: "Accidental Death / Permanent Disability",
+        purchaseDate: new Date(2017, 5, 26, 16, 43),
+        paid: 10.99
+      }
+    ];
+    const claims = [
+      {
+        key: "CL11",
+        status: "pending",
         name:
           "Accidental Death / Permanent Disability with Medical Reimbursement",
-        date: new Date(2017, 5, 26, 16, 43)
+        purchaseDate: new Date(2017, 3, 26, 16, 43),
+        claimDate: new Date(2017, 5, 26, 16, 43),
+        claimAmount: 1000
       }
     ];
-    const pendingPolicies = [
-      {
-        key: 1,
-        status: "pending",
-        name: "Accidental Death / Permanent Disability",
-        date: new Date(2017, 5, 26, 16, 43),
-        claimAmount: 500
-      }
-    ];
-    const rejectedPolicies = [
-      {
-        key: 1,
-        status: "rejected",
-        name: "Accidental Death / Permanent Disability",
-        date: new Date(2017, 5, 26, 16, 43),
-        claimAmount: 500
-      }
-    ];
-
     return (
       <View style={styles.container}>
         <SectionList
@@ -107,9 +115,18 @@ export default class StatusScreen extends Component {
           renderSectionHeader={this.renderSectionHeader}
           sections={[
             // homogenous rendering between sections
-            { data: activePolicies, key: "active" },
-            { data: pendingPolicies, key: "pending" },
-            { data: rejectedPolicies, key: "rejected" }
+            {
+              data: policies,
+              title: "POLICIES",
+              key: "policies",
+              renderItem: (...params) => this.renderItem("policies", ...params)
+            },
+            {
+              data: claims,
+              title: "CLAIMS",
+              key: "claims",
+              renderItem: (...params) => this.renderItem("claims", ...params)
+            }
           ]}
         />
       </View>
@@ -118,6 +135,18 @@ export default class StatusScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+  policyStatusText: { fontWeight: "400" },
+  policyStatusTextActive: { color: "green" },
+  policyStatusTextExpiring: { color: "#FFC107" },
+  policyStatusTextRejected: { color: "red" },
+  policyContent: {
+    flex: 1
+  },
+  policyStatus: {
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center"
+  },
   date: {
     marginTop: 5,
     fontSize: 15
@@ -130,7 +159,10 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1
   },
   policy: {
-    paddingHorizontal: 15,
+    flex: 1,
+    flexDirection: "row",
+    paddingLeft: 20,
+    paddingRight: 5,
     paddingVertical: 20
   },
   rejectedSectionHeader: {
