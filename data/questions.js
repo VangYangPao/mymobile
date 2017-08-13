94838bad4f08eeabdd40c91eebe8302eaed96aeb
mavenave <guanhao3797@gmail.com>
2017-08-14 03:33:39 +0800

    5 options for travel implemented so far
    missing:
    - datetime picker in multiinput
    - table images
    - table for items

--- data/questions.js
+++ data/questions.js
@@ -551,8 +551,147 @@ export const travelClaimQuestions = [
   {
     question: "I’m so sorry to hear that. I assume you are FULLNAME’s claimant/next of kin. Please share with me the date and time of the accident",
     responseType: "date",
-    id: "accidentDate",
+    id: "claimDate",
     include: ["death"]
+  },
+  {
+    question: "Oh… I am sad to hear that. Let me help out on the claim fast. Please share with me the date and time of the accident",
+    responseType: "date",
+    id: "claimDate",
+    include: ["permanentDisability"]
+  },
+  {
+    question: "FULLNAME, I wish you get well soon. Let me help out on the claim fast. Please share with me the date and time of the accident",
+    responseType: "date",
+    id: "claimDate",
+    include: ["medicalReimbursement"]
+  },
+  {
+    question: "FULLNAME, you just selected the option to claim for damaged or loss of baggage while in the custody of others. Please share with me the date and time of damage or loss",
+    responseType: "date",
+    id: "claimDate",
+    include: ["baggageDamaged"]
+  },
+  {
+    question: "FULLNAME, you just selected the option to claim for loss of personal document. Please share with me the date and time of loss",
+    responseType: "date",
+    id: "claimDate",
+    include: ["lossOfPersonalDocument"]
+  },
+  {
+    question: "FULLNAME, you just selected the option to claim for travel delay, baggage delay, or flight misconnection. Please share with me the flight details",
+    responseType: ["string", "string"],
+    labels: ["Airline company", "Flight number"],
+    id: ["airlineCompany", "flightNo"],
+    include: ["travelDelay"]
+  },
+  {
+    question: "What is the cause of travel delay, baggage delay, or flight misconnection",
+    responseType: "string",
+    responseLength: 600,
+    id: "travelDelayCause",
+    include: ["travelDelay"]
+  },
+  {
+    question: "What is the amount you intend to claim?",
+    responseType: "number",
+    id: "claimAmount",
+    include: ["travelDelay"]
+  },
+  {
+    question: "Have you claimed from the airline or other source?",
+    responseType: ["boolean", "choice"],
+    choices: [
+      {
+        label: "Yes",
+        value: true
+      },
+      { label: "No", value: false }
+    ],
+    id: "hasClaimedFromOtherSource",
+    include: ["travelDelay"]
+  },
+
+  {
+    question: "What is the amount you claimed?",
+    responseType: "number",
+    id: "claimAmountFromOtherSource",
+    include: ["travelDelay"]
+  },
+  {
+    question: "What is the ORIGINAL departure date, time and departing airport?",
+    responseType: ["date", "string"],
+    labels: ["Original Departure Date", "Departure Airport"],
+    id: ["originalFlightDepartureDate", "originalFlightDepartureAirport"],
+    include: ["travelDelay"]
+  },
+  {
+    question: "What is the ACTUAL departure date, time and departing airport?",
+    responseType: ["date", "string"],
+    labels: ["Actual Departure Date", "Departure Airport"],
+    id: ["actualFlightDepartureDate", "actualFlightDepartureAirport"],
+    include: ["travelDelay"]
+  },
+  {
+    question: "Is your baggage delayed?",
+    responseType: ["boolean", "choice"],
+    choices: [
+      {
+        label: "Yes",
+        value: true
+      },
+      { label: "No", value: false }
+    ],
+    id: "isBaggageDelayed",
+    include: ["travelDelay"]
+  },
+  {
+    question: "What is your baggage's ORIGINAL departure date, time and departing airport? ",
+    responseType: ["date", "string"],
+    labels: ["Original Departure Date", "Departure Airport"],
+    id: ["originalBaggageDepartureDate", "originalBaggageDepartureAirport"],
+    include: ["travelDelay"],
+    condition: "this.state.answers.isBaggageDelayed"
+  },
+  {
+    question: "What is your baggage's ACTUAL departure date, time and departing airport? ",
+    responseType: ["date", "string"],
+    labels: ["Actual Departure Date", "Departure Airport"],
+    id: ["actualBaggageDepartureDate", "actualBaggageDepartureAirport"],
+    include: ["travelDelay"],
+    condition: "this.state.answers.isBaggageDelayed"
+  },
+  {
+    question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
+    responseType: "imageTable",
+    columns: [
+      {
+        label: "Scheduled and revised flight itinerary, or boarding pass",
+        id: "boardingPass"
+      },
+      {
+        label: "Written document from airline company on the on the duration of travel (and/or baggage) delay and the cause of travel (and/or baggage) delay",
+        id: "delayDocumentFromAirline"
+      },
+      {
+        label: "Original receipt for additional travelling and/or hotel accommodation ",
+        id: "travelReceipt"
+      },
+      {
+        label: "Document stating the compensation from airlines and/or other sources ",
+        id: "compensationDocument"
+      },
+      {
+        label: "Written document from airline company on the on the cause of flight misconnection, and stating there is no onward transportation available to you within 6 consecutive hours of your arrival time (if any)",
+        id: "flightMisconnectionDocument"
+      },
+      {
+        label: "Property irregularity report, and acknowledgement receipt of baggage received (if any)",
+        id: "irregularityReport"
+      }
+    ],
+    id: "travelDelayImages",
+    include: ["travelDelay"]
   },
   {
     question: "Where did it happen?",
@@ -563,16 +702,103 @@ export const travelClaimQuestions = [
     question: "What happened in detail? What is the cause of the accident?",
     responseType: "string",
     responseLength: 600,
-    id: "description"
+    id: "description",
+    exclude: ["baggageDamaged"]
   },
-
+  {
+    question: "What happened in detail (leading to damage or loss)?",
+    responseType: "string",
+    responseLength: 600,
+    id: "description",
+    include: ["baggageDamaged"]
+  },
+  {
+    question: "If the damage or loss happened whilst baggage was in transit or in custody, have any steps been taken to claim against them?",
+    responseType: ["boolean", "choice"],
+    choices: [
+      {
+        label: "Yes",
+        value: true
+      },
+      { label: "No", value: false }
+    ],
+    id: "stepsTakenToClaim",
+    include: ["baggageDamaged", "lossOfPersonalDocument"]
+  },
+  {
+    question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
+    responseType: "imageTable",
+    columns: [
+      { label: "Boarding pass/Flight itinerary", id: "boardingPass" },
+      {
+        label: "Original receipt for replacement of personal document",
+        id: "replacementReceipt"
+      },
+      {
+        label: "Original receipt for additional travelling and/or hotel accommodation ",
+        id: "travelReceipt"
+      },
+      {
+        label: "Document stating the compensation from airlines and/or other sources ",
+        id: "compensationDocument"
+      },
+      {
+        label: "Police report lodged within 24 hours (if any)",
+        id: "policeReport"
+      }
+    ],
+    id: "lossOfPersonalDocumentImages",
+    include: ["lossOfPersonalDocument"]
+  },
+  {
+    question: "We are almost done here. I need your help to itemise what are lost or damaged",
+    responseType: "table",
+    columns: [
+      {
+        label: "Description (make and model)",
+        id: "description",
+        type: "string"
+      },
+      { label: "Date of Purchase", id: "dateOfPurchase", type: "date" },
+      { label: "Place of Purchase", id: "placeOfPurchase", type: "string" },
+      { label: "Original Price", id: "originalPrice", type: "number" },
+      { label: "Claim Amount", id: "claimAmount", type: "number" }
+    ],
+    id: "baggageItems",
+    include: ["baggageDamaged"]
+  },
+  {
+    question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
+    responseType: "imageTable",
+    columns: [
+      { label: "Boarding pass/Flight itinerary", id: "boardingPass" },
+      {
+        label: "Original purchase receipts or Warranty cards",
+        id: "purchaseReceipts"
+      },
+      { label: "Photos showing the damage", id: "damagePhotos" },
+      {
+        label: "Document stating the compensation from airlines and/or other sources ",
+        id: "compensationDocument"
+      },
+      {
+        label: "Property irregularity report (if any)",
+        id: "irregularityReport"
+      },
+      {
+        label: "Police report lodged within 24 hours (if any)",
+        id: "policeReport"
+      }
+    ],
+    id: "baggageDamagedImages",
+    include: ["baggageDamaged"]
+  },
   {
     question: "What is the injury you suffered? What is the extent of your injury?",
     responseType: "string",
     responseLength: 500,
     include: ["permanentDisability", "medicalReimbursement"]
   },
-
   {
     question: "Try to recall for a moment, have you suffered the same injury before? ",
     responseType: ["boolean", "choice"],
@@ -693,7 +919,8 @@ export const travelClaimQuestions = [
   },
   {
     question: "FULLNAME, to complete your claim, I need your help to post the ORIGINAL MEDICAL BILLS AND/OR RECEIPTS to: HLAS, 11 Keppel Road #11-01 ABI Plaza Singapore 089057, within 48 hours ",
-    responseType: null
+    responseType: null,
+    include: ["permanentDisability", "medicalReimbursement"]
   },
   {
     question: "If you have submitted the original bills and/or receipts to other insurer or your employer, please snap a clear photo of reimbursement letter, or discharge voucher from insurer, or letter from employer indicating the amount paid to you. Either one will do.",
@@ -702,34 +929,62 @@ export const travelClaimQuestions = [
     id: "reimbursementLetter",
     include: ["permanentDisability"]
   },
-  {
-    question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
-    responseType: "images",
-    labels: [
-      "Boarding pass/Flight itinerary",
-      "Original medical bills/receipts (if any)",
-      "Discharge summary",
-      "Photocopy medical bills/receipts if original is submitted to other insurer",
-      "Medical report",
-      "Police report (if any)"
-    ],
-    include: ["medicalReimbursement"],
-    id: "medicalReimbursementImages"
-  },
-  {
-    question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
-    responseType: "images",
-    labels: [
-      "Boarding pass/Flight itinerary",
-      "Death certificate that is certified true copy by your lawyer or any notary public",
-      "Letter from Immigration and Checkpoint Authority, ICA",
-      "Repatriation report",
-      "Document proof of claimant’s relationship to the person who died",
-      "Police report (if any)"
-    ],
-    include: ["death"],
-    id: "deathClaimImages"
-  },
+
+  // {
+  //   question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
+  //   responseType: "imageTable",
+  //   columns: [
+  //     { label: "Boarding pass/Flight itinerary", id: "boardingPass" },
+  //     {
+  //       label: "Original medical bills/receipts (if any)",
+  //       id: "medicalBill"
+  //     },
+  //     { label: "Discharge summary", id: "dischargeSummary" },
+  //     {
+  //       label: "Photocopy medical bills/receipts if original is submitted to other insurer",
+  //       id: "photocopyMedicalBill"
+  //     },
+  //     {
+  //       label: "Medical report",
+  //       id: "medicalReport"
+  //     },
+  //     {
+  //       label: "Police report (if any)",
+  //       id: "policeReport"
+  //     }
+  //   ],
+  //   include: ["medicalReimbursement"],
+  //   id: "medicalReimbursementImages"
+  // },
+  // {
+  //   question: "We are almost done to get you claim fast. I need your help to snap or upload some photos. Refer to the boxes below, try your best to snap/upload the right images for each box",
+  //   responseType: "imageTable",
+  //   columns: [
+  //     { label: "Boarding pass/Flight itinerary", id: "boardingPass" },
+  //     {
+  //       label: "Death certificate that is certified true copy by your lawyer or any notary public",
+  //       id: "deathCertificate"
+  //     },
+  //     {
+  //       label: "Letter from Immigration and Checkpoint Authority, ICA",
+  //       id: "immigrationLetter"
+  //     },
+  //     {
+  //       label: "Repatriation report",
+  //       id: "repatriationReport"
+  //     },
+  //     {
+  //       label: "Document proof of claimant’s relationship to the person who died",
+  //       id: "relationshipProof"
+  //     },
+  //     {
+  //       label: "Police report (if any)",
+  //       id: "policeReport"
+  //     }
+  //   ],
+  //   include: ["death"],
+  //   id: "deathClaimImages"
+  // },
   {
     question: "Thank you for your patience. Please keep this phone with you at all times, as I shall send you notifications and messages on your claim. Please switch on the notification. ",
     responseType: null
