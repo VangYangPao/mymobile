7cd8b2a6b52c1bf6f5b4d2067f62f2dc09a7f659
mavenave <guanhao3797@gmail.com>
2017-10-31 15:33:24 +0800

    removed microumbrella-core

--- microumbrella-core/styles/colors.js
+++
@@ -1,8 +0,0 @@
-export default (COLORS = {
-  primaryOrange: "#FB8C00",
-  primaryText: "#424242",
-  borderLine: "#BDBDBD",
-  softBorderLine: "#EEEEEE",
-  tertiaryGreen: "#8BC34A",
-  errorRed: "#F44336"
-});
