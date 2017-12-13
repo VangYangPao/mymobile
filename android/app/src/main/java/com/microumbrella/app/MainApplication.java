3298151f63d7a23f775431e8f372dedd902b2932
mavenave <guanhao3797@gmail.com>
2017-12-13 12:36:05 +0800

    added react-native-device-info

--- android/app/src/main/java/com/microumbrella/app/MainApplication.java
+++ android/app/src/main/java/com/microumbrella/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.microumbrella.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.appsee.reactnative.AppseeReactPackage;
 import com.smixx.fabric.FabricPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new AppseeReactPackage(),
             new FabricPackage(),
             new ReactNativeOneSignalPackage(),
